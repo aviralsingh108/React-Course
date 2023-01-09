78457c5db125c8693e26b6e50bc70d5e14a7df0f
aviralsingh108 <admiralaviral@gmail.com>
2023-01-10 00:44:27 +0530

    JSX and Functional Components

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1 = React.createElement("h1", { id: "heading1" }, "First heading");
 const heading2 = React.createElement(
   "h2",
@@ -11,3 +14,22 @@ const container = React.createElement("div", { id: "container" }, [
 console.log(container);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
+
+// JSX
+
+const headingNew = <h1>This is a heading with JSX</h1>;
+
+const root2 = ReactDOM.createRoot(document.getElementById("root"));
+
+root2.render(headingNew);
+
+// Functional Components
+
+const HeaderComponent = () => {
+  return <h1>Heading with a functional component</h1>;
+};
+
+const root3 = ReactDOM.createRoot(document.getElementById("root"));
+// root3.render(HeaderComponent());  //Another Way of doing it
+
+root3.render(<HeaderComponent />);
